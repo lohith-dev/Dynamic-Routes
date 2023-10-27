302932a0de1da74f17c6cf0d1c94e155e4ab5360
lohith-dev <srlohith92@gmail.com>
2023-10-27 23:28:31 +0530

    added cart and delete cart

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
-const User = require('./models/User');
+const User = require('./models/User.js');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -20,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findOne({where:{id:1}})
+  User.findOne({where:{id:1}})
     .then(user => {
       req.user = user;
       next();
@@ -35,13 +37,17 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  // .sync({ force: true })
+//   .sync({ force: true })
   .sync()
   .then(result => {
     return User.findOne({where:{id:1}});
-    // console.log(result);
+    
   })
   .then(user => {
     if (!user) {
@@ -50,7 +56,10 @@ sequelize
     return user;
   })
   .then(user => {
-    // console.log(user);
+    
+      return user.createCart();
+  })
+  .then(cart => {
     app.listen(3000);
   })
   .catch(err => {
